da28b78f290eb84c4c5ded3d3cab73ae7980da05
AluminumAlman <Almantas1122334455@gmail.com>
2022-12-09 10:33:14 +0200

    Library's code refactoring

--- lib/mvsdtd/src/filestring.c
+++ lib/mvsdtd/src/filestring.c
@@ -4,15 +4,27 @@
 #include "string.h"
 #include "memalign.h"
 
+size_t getFileSize(FILE *file)
+{
+	size_t fileSize,
+               oldPosition = ftell(file);
+	if(oldPosition == (size_t)-1)
+		return(0);
+
+	fseek(file, 0, SEEK_END);
+	fileSize = ftell(file);
+	fseek(file, oldPosition, SEEK_SET);
+
+	return(fileSize);
+}
+
 char *readFileToString(char *const fileName)
 {
 	FILE *inputFile = fopen(fileName, "rb");
 	if(inputFile == NULL)
 		return(NULL);
 
-	fseek(inputFile, 0, SEEK_END);
-	size_t fileSize = ftell(inputFile);
-	fseek(inputFile, 0, SEEK_SET);
+	size_t fileSize = getFileSize(inputFile);
 
 	char *newString = malloc(alignNumberToMemory(fileSize+1));
 	if(newString == NULL)
@@ -43,14 +55,15 @@ char* stringCopy(char *destinationString, char *const sourceString)
 {
 	for(size_t i = 0; (destinationString[i] = sourceString[i]) != 0; ++i)
 		;
+
 	return(destinationString);
 }
 
 char* stringCopyInvertedDirection(char *destinationString, char *const sourceString)
 {
 	// Start copying from the right and proceed left
-	for(size_t i = strlen(sourceString) + 1; i > 0; --i)
-		destinationString[i-1] = sourceString[i-1];
+	for(size_t i = strlen(sourceString); i != (size_t)-1; --i)
+		destinationString[i] = sourceString[i];
 
 	return(destinationString);
 }
@@ -64,40 +77,40 @@ char* replaceStringTokenWithSubstitute(char *stringTokenStart, size_t stringToke
 	else if(stringTokenLength > tokenSubstituteLength) // Shrink the string to correctly fit the substitute
 		stringCopy(stringTokenStart+tokenSubstituteLength, stringTokenStart+stringTokenLength);
 
-	memcpy(stringTokenStart, tokenSubstituteString, tokenSubstituteLength);
+	memcpy(stringTokenStart, tokenSubstituteString, tokenSubstituteLength); // Perform the substitution
 	return(stringTokenStart);
 }
 
 
 char *getSubstitutedString(char const *sourceString, size_t substituteCount, char *const substituteStrings[])
 {
-	// Calculate the new string length that would definitely fit everything
+	if(substituteStrings == NULL)
+		return(NULL);
+
+	// Calculate the sum of all string lengths, which is used for memory allocation
 	size_t newStringLength = strlen(sourceString);
-	if(substituteStrings != NULL)
-		for(size_t i = 0; i < substituteCount; ++i)
-			if(substituteStrings[i] != NULL)
-				newStringLength += strlen(substituteStrings[i]);
+	for(size_t i = 0; i < substituteCount; ++i)
+		if(substituteStrings[i] != NULL)
+			newStringLength += strlen(substituteStrings[i]);
 
 	char *newString = malloc(alignNumberToMemory(newStringLength+1));
 	if(newString == NULL)
 		return(NULL);
-
 	strcpy(newString, sourceString);
-
-	if(substituteStrings == NULL)
-		return(newString);
-
-
+	
+	// Search for and perform substitutions
 	char needleString[32] = "";
 	for(size_t i = 0; i < substituteCount; ++i)
 	{
 		if(substituteStrings[i] == NULL)
 			continue;
 
+		// Search setup
 		sprintf(needleString, "${%lu}", i);
 		size_t needleStringLength = strlen(needleString);
 		char *newStringPosition = newString;
 
+		// Search and substitute loop
 		while((newStringPosition = strstr(newStringPosition, needleString)) != NULL)
 			replaceStringTokenWithSubstitute(newStringPosition, needleStringLength, substituteStrings[i]);
 	}
