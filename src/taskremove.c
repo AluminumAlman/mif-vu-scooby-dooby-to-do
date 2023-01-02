9033093a895c6d9a8bff956b5f72184ba0dcfda5
niasenago <118674771+niasenago@users.noreply.github.com>
2023-01-02 20:40:18 +0200

    Update taskremove.c

--- src/taskremove.c
+++ src/taskremove.c
@@ -1,4 +1,22 @@
-void TaskRemove(char name[])
-{
-        return;
+#pragma once
+#include <string.h>
+#include "common.h"
+
+
+int TaskRemove (Task taskArray[], int size, Task taskToDelete ){       
+        int position;
+        
+        for(int i = 0; i < size; ++i){
+                if(strcmp(taskArray[i].name, taskToDelete.name ) == 0){
+                        position = i;
+                        break;
+                }
+                return -1;       
+        }
+
+        for(int i = position; i < size - 1; ++i){
+                taskArray[i] = taskArray[i + 1];  
+        }
+        return 0;
 }
+// size maine turi sumazeti
