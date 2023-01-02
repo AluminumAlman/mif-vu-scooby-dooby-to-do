f8a236a1c88acfd538a55aeb2a6f066fff285e75
niasenago <118674771+niasenago@users.noreply.github.com>
2023-01-02 20:55:20 +0200

    Update taskremove.c

--- src/taskremove.c
+++ src/taskremove.c
@@ -4,7 +4,7 @@
 #include "taskremove.h"
 
 
-int TaskRemove (Task taskArray[], int size, Task taskToDelete ){      //funkcija grazina sekmes atveju 0, nesekmes -1 
+int TaskRemove (Task taskArray[], int size, Task taskToDelete ){      //funkcija grazina sekmes atveju 1, nesekmes 0 
         int position;
         int flag = 0;           //flagas skirtas patikrinti, ar buvo rastas task'as, kuri reikia istrinti 
         for(int i = 0; i < size; ++i){
@@ -16,11 +16,11 @@ int TaskRemove (Task taskArray[], int size, Task taskToDelete ){      //funkcija
                    
         }
         if(!flag)
-                return -1;      //masyve nebuvo rasta reikiamo task'o
+                return 0;      //masyve nebuvo rasta reikiamo task'o
 
         for(int i = position; i < size - 1; ++i){
                 taskArray[i] = taskArray[i + 1];  
         }
-        return 0;               //taskas buvo sekmingai istrintas
+        return 1;               //taskas buvo sekmingai istrintas
 }
 // size main'e turi sumazeti
