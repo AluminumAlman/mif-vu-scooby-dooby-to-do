39c4482f307989ad460c2f9ef5560c3c985721a1
niasenago <118674771+niasenago@users.noreply.github.com>
2023-01-11 16:21:46 +0200

    Update taskadd.h

--- src/taskadd.h
+++ src/taskadd.h
@@ -1,7 +1,7 @@
 #ifndef TASK_ADD_H
 #define TASK_ADD_H
 
-extern char TaskAdd(char argumentFile[], Task allTasks[], int newTaskIndex)
+extern char TaskAdd(char argumentFile[], Task allTasks[], int newTaskIndex); //function returns 1 if task is successifully added, 0 if error occured. 
 
 
 #endif
