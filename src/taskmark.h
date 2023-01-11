d1f5597aea9b9172468c95468059a3fe6e1e544d
niasenago <118674771+niasenago@users.noreply.github.com>
2023-01-11 14:59:00 +0200

    Update taskmark.h

--- src/taskmark.h
+++ src/taskmark.h
@@ -1,6 +1,6 @@
 #ifndef TASK_MARK_H
 #define TASK_MARK_H
 
-extern char TaskMark(char taskName[], Task taskList[], int taskAmount);   //if task is successfully marked, then function returns 1, otherwise 0 
+extern char TaskMark(char taskName[], Task taskList[], int taskAmount);   //if task is successfully marked, then function returns 1, otherwise 0. 
 
 #endif
